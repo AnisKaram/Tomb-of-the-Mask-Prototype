4bd1cb270f7ce18a9d0c07ed448bca0378513e87
Anis karam <anisTkaram@hotmail.com>
2024-09-17 23:26:53 +0300

    Swipe mechanic fixed and other modifications.
    - I added a new swipe solution in the SwipeDetectorManager using the delta of the finger and commented the first solution that uses the dot product.
    - In the PlayerController script I moved the BoxCast in a separate bool method and cleaned up the script by changing fields names and changed the groundMask field to an integer field with initializing it in the Awake method, changed the thrust value.

--- Assets/Scripts/Input Detection/SwipeDetectorManager.cs
+++ Assets/Scripts/Input Detection/SwipeDetectorManager.cs
@@ -1,13 +1,12 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-
 public enum SwipeDirections
 {
-    Left,
-    Right,
-    Up,
-    Down,
+    left,
+    right,
+    up,
+    down,
     Null
 }
 
@@ -17,16 +16,17 @@ public class SwipeDetectorManager : MonoBehaviour
     #region Fields
     private Inputs m_inputs;
 
-    public int m_isTouchStarted;
+    private int m_isTouchStarted;
     private SwipeDirections m_swipeDirection;
-    private Vector2 m_touchStartPosition;
+    private float m_deltaThreshold = 2f;
 
-    private const float m_swipeThreshold = 0.9f;
+    //private Vector2 m_touchStartPosition;
+    //private const float m_swipeThreshold = 0.1f;
     #endregion
 
 
     #region Events
-    public static event UnityAction<SwipeDirections> SwipeDetected;
+    public static event UnityAction<SwipeDirections, Vector2> SwipeDetected;
     #endregion
 
 
@@ -46,37 +46,71 @@ public class SwipeDetectorManager : MonoBehaviour
     {
         if (BinaryUtil.BitmaskAND(m_isTouchStarted, 1))
         {
-            Vector2 touchPosition = m_inputs.Gameplay.Touch.ReadValue<Vector2>();
-            Vector2 direction2D = (touchPosition - m_touchStartPosition).normalized;
+            // Solution n.2
+            Vector2 swipeDelta = m_inputs.Gameplay.Swipe.ReadValue<Vector2>();
 
-            // Dot product
-            if (/*m_swipeDirection != SwipeDirections.Right && */Vector2.Dot(Vector2.right, direction2D) > m_swipeThreshold) // Swipe right.
+            if (swipeDelta.x != 0f && swipeDelta.y != 0f) { return; } // filter the delta of the finger
+
+            if (swipeDelta.x < -m_deltaThreshold)
             {
-                Debug.Log($"RIGHT");
-                m_swipeDirection = SwipeDirections.Right;
-                InvokeSwipeEvent(m_swipeDirection);
-                //return;
+                Debug.Log($"left swipe");
+                m_swipeDirection = SwipeDirections.left;
+                InvokeSwipeEvent(m_swipeDirection, Vector2.left);
             }
-            else if (/*m_swipeDirection != SwipeDirections.Left && */Vector2.Dot(Vector2.left, direction2D) > m_swipeThreshold) // Swipe left.
+            if (swipeDelta.x > m_deltaThreshold)
             {
-                Debug.Log($"LEFT");
-                m_swipeDirection = SwipeDirections.Left;
-                InvokeSwipeEvent(m_swipeDirection);
-                //return;
+                Debug.Log($"right swipe");
+                m_swipeDirection = SwipeDirections.right;
+                InvokeSwipeEvent(m_swipeDirection, Vector2.right);
             }
-            else if (/*m_swipeDirection != SwipeDirections.Up && */Vector2.Dot(Vector2.up, direction2D) > m_swipeThreshold) // Swipe up.
+            if (swipeDelta.y < -m_deltaThreshold)
             {
-                Debug.Log($"UP");
-                m_swipeDirection = SwipeDirections.Up;
-                InvokeSwipeEvent(m_swipeDirection);
-                //return;
+                Debug.Log($"down swipe");
+                m_swipeDirection = SwipeDirections.down;
+                InvokeSwipeEvent(m_swipeDirection, Vector2.down);
             }
-            else if (/*m_swipeDirection != SwipeDirections.Down && */ Vector2.Dot(Vector2.down, direction2D) > m_swipeThreshold) // Swipe down.
+            if (swipeDelta.y > m_deltaThreshold)
             {
-                Debug.Log($"DOWN");
-                m_swipeDirection = SwipeDirections.Down;
-                InvokeSwipeEvent(m_swipeDirection);
+                Debug.Log($"up swipe");
+                m_swipeDirection = SwipeDirections.up;
+                InvokeSwipeEvent(m_swipeDirection, Vector2.up);
             }
+            return;
+
+            #region Solution n.1
+            //
+            //Vector2 touchPosition = m_inputs.Gameplay.Touch.ReadValue<Vector2>();
+            //Vector2 direction2D = (touchPosition - m_touchStartPosition).normalized;
+
+            //// Dot product
+            //if (/*m_swipeDirection != SwipeDirections.Right && */Vector2.Dot(Vector2.right, direction2D) > m_swipeThreshold) // Swipe right.
+            //{
+            //    m_swipeDirection = SwipeDirections.right;
+            //    InvokeSwipeEvent(m_swipeDirection, Vector2.right);
+            //    text.text = $"RIGHT";
+            //    //return;
+            //}
+            //if (/*m_swipeDirection != SwipeDirections.Left && */Vector2.Dot(Vector2.left, direction2D) > m_swipeThreshold) // Swipe left.
+            //{
+            //    m_swipeDirection = SwipeDirections.left;
+            //    InvokeSwipeEvent(m_swipeDirection, Vector2.left);
+            //    text.text = $"LEFT";
+            //    //return;
+            //}
+            //if (/*m_swipeDirection != SwipeDirections.Up && */Vector2.Dot(Vector2.up, direction2D) > m_swipeThreshold) // Swipe up.
+            //{
+            //    m_swipeDirection = SwipeDirections.up;
+            //    InvokeSwipeEvent(m_swipeDirection, Vector2.up);
+            //    text.text = $"UP";
+            //    //return;
+            //}
+            //if (/*m_swipeDirection != SwipeDirections.Down && */ Vector2.Dot(Vector2.down, direction2D) > m_swipeThreshold) // Swipe down.
+            //{
+            //    m_swipeDirection = SwipeDirections.down;
+            //    InvokeSwipeEvent(m_swipeDirection, Vector2.down);
+            //    text.text = $"DOWN";
+            //}
+            #endregion
         }
     }
     private void OnDestroy()
@@ -93,16 +127,16 @@ public class SwipeDetectorManager : MonoBehaviour
     private void OnTouchStarted()
     {
         m_isTouchStarted = BinaryUtil.SetFlag(m_isTouchStarted, 1);
-        m_touchStartPosition = m_inputs.Gameplay.Touch.ReadValue<Vector2>();
+        //m_touchStartPosition = m_inputs.Gameplay.Touch.ReadValue<Vector2>();
     }
     private void OnTouchCanceled()
     {
         m_isTouchStarted = BinaryUtil.RemoveFlag(m_isTouchStarted, 1);
     }
 
-    private void InvokeSwipeEvent(SwipeDirections swipe)
+    private void InvokeSwipeEvent(SwipeDirections swipe, Vector2 direction)
     {
-        SwipeDetected?.Invoke(swipe);
+        SwipeDetected?.Invoke(swipe, direction);
     }
     #endregion
 }
--- Assets/Scripts/Player Controller/PlayerController.cs
+++ Assets/Scripts/Player Controller/PlayerController.cs
@@ -7,15 +7,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D m_rigidbody2D;
     private BoxCollider2D m_boxCollider2D;
 
-    public bool canDash;
+    public bool isDashing;
 
     public Vector3 m_dashDirection;
+    public Vector2 m_castDirection;
     public SwipeDirections m_swipeDirection;
-    public float m_thurst = 0.05f;
+    private float m_thrust = 0.2f;
 
-    public LayerMask m_groundMask;
-
-    public float distance;
+    private int m_groundMask;
 
     private void Awake()
     {
@@ -24,96 +23,95 @@ public class PlayerController : MonoBehaviour
 
         m_swipeDirection = SwipeDirections.Null;
 
-        // testing
-        distance = m_boxCollider2D.bounds.extents.x;
+        m_groundMask = (1 << 10);
 
         SwipeDetectorManager.SwipeDetected += OnSwipeDetected;
     }
-
     private void OnDestroy()
     {
         SwipeDetectorManager.SwipeDetected -= OnSwipeDetected;
     }
-
+    // TODO Delete after done testing
     private void Update()
     {
+        if (isDashing) { return; }
+
         if (Input.GetKeyDown(KeyCode.A))
         {
-            OnSwipeDetected(SwipeDirections.Left);
+            OnSwipeDetected(SwipeDirections.left, Vector2.left);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            OnSwipeDetected(SwipeDirections.Right);
+            OnSwipeDetected(SwipeDirections.right, Vector2.right);
         }
 
         if (Input.GetKeyDown(KeyCode.W))
         {
-            OnSwipeDetected(SwipeDirections.Up);
+            OnSwipeDetected(SwipeDirections.up, Vector2.up);
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            OnSwipeDetected(SwipeDirections.Down);
+            OnSwipeDetected(SwipeDirections.down, Vector2.down);
         }
     }
 
-    private void OnSwipeDetected(SwipeDirections swipeDirection)
+    private void OnSwipeDetected(SwipeDirections swipeDirection, Vector2 direction)
     {
-        if (canDash) { return; }
-
-        if (swipeDirection == m_swipeDirection) { return; }
-
+        if (swipeDirection == m_swipeDirection) { return; } // avoid same swipe direction
+        
+        m_castDirection = direction;
         m_swipeDirection = swipeDirection;
 
         // Dash direction
-        if (swipeDirection == SwipeDirections.Left || swipeDirection == SwipeDirections.Right)
+        if (swipeDirection == SwipeDirections.left || swipeDirection == SwipeDirections.right)
         {
             m_dashDirection = transform.right;
         }
-        else if (swipeDirection == SwipeDirections.Up || swipeDirection == SwipeDirections.Down)
+        else if (swipeDirection == SwipeDirections.up || swipeDirection == SwipeDirections.down)
         {
             m_dashDirection = transform.up;
         }
 
         // Thrust
-        if (swipeDirection == SwipeDirections.Up || swipeDirection == SwipeDirections.Right)
+        if (swipeDirection == SwipeDirections.up || swipeDirection == SwipeDirections.right)
         {
-            m_thurst = 0.05f;
+            m_thrust = 0.2f;
         }
-        else if (swipeDirection == SwipeDirections.Down || swipeDirection == SwipeDirections.Left)
+        else if (swipeDirection == SwipeDirections.down || swipeDirection == SwipeDirections.left)
         {
-            m_thurst = -0.05f;
+            m_thrust = -0.2f;
         }
 
-        canDash = true;
+        isDashing = true;
     }
 
     // -----
     // TODO Move the code to the states and StateMachine
     // -----
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        if (canDash)
+        if (isDashing)
         {
             DashPlayer();
-
-            bool idle = Physics2D.BoxCast(m_boxCollider2D.bounds.center, m_boxCollider2D.bounds.size, 0f, Vector2.left, m_boxCollider2D.bounds.extents.x, m_groundMask);
-            Debug.Log($"is idle: {idle}");
+            if (IsPlayerIdle()) { isDashing = false; }
         }
     }
 
-    // Direction is transform.up or right
     private void DashPlayer()
     {
-        m_rigidbody2D.AddForce(m_dashDirection * m_thurst, ForceMode2D.Force);
+        m_rigidbody2D.AddForce(m_dashDirection * m_thrust, ForceMode2D.Force);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private bool IsPlayerIdle()
     {
-        if (canDash && collision.collider.CompareTag("Ground"))
-        {
-            //canDash = false;
-        }
+        bool idle = Physics2D.BoxCast(
+            origin: m_boxCollider2D.bounds.center,
+            size: m_boxCollider2D.bounds.size,
+            angle: 0f,
+            direction: m_castDirection,
+            distance: m_boxCollider2D.bounds.extents.x,
+            layerMask: m_groundMask);
+        return idle;
     }
 }
