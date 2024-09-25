3a768e46adb6c4fa0784a95d2343ccc35a06db3c
Anis karam <anisTkaram@hotmail.com>
2024-09-25 22:13:33 +0300

    Camera Shake
    - Made some improvements to the CameraShake script and cleaned up the script.

--- Assets/Scripts/Camera/CameraShake.cs
+++ Assets/Scripts/Camera/CameraShake.cs
@@ -1,56 +1,49 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+public enum ShakeAxis { Xonly, Yonly, XY }
 
 public class CameraShake : MonoBehaviour
 {
-    public Vector3 m_initialPosition;
-    public float m_elapsedShakingTime = 0f;
-    public float m_shakingTime = 0f;
-    [Range(0, 1)] public float m_smoothness = 0f;
-    public float m_shakeIntensity = 0f;
+    #region Fields
+    [Header("Shake Settings")]
+    [SerializeField] private float m_shakingTime = 0f;
+    [SerializeField] private float m_shakeIntensity = 0f;
+    [SerializeField] private ShakeAxis m_shakeAxis;
+    #endregion
 
-    private void Update()
+
+    #region Public Methods
+    public void ShakeCamera()
     {
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            ShakeCamera(shakingTime: .05f);
-        }
-    }
-    
-    private void ShakeCamera(float shakingTime)
-    {
-        m_initialPosition = transform.position;
         StartCoroutine(CameraShakeCoroutine());
     }
+    #endregion
+
+
+    #region Private Methods
     private IEnumerator CameraShakeCoroutine()
     {
-        m_elapsedShakingTime = 0f;
+        float elapsedShakingTime = 0f;
+        Vector3 initialPosition = transform.position;
 
-        while (m_elapsedShakingTime <= m_shakingTime)
+        while (elapsedShakingTime < m_shakingTime)
         {
-            // 2
-            Vector2 randomPoint = (Vector2)m_initialPosition + Random.insideUnitCircle * m_shakeIntensity;
-            float randomXPoint = Random.Range(-0.15f, 0.15f) * m_shakeIntensity;
-            Vector3 randomPointVect3 = new Vector3(randomXPoint, transform.position.y, -10f);
-            transform.position = Vector3.Lerp(transform.position, randomPointVect3, Time.deltaTime * m_smoothness);
+            Vector3 shakePosition = Vector3.zero;
+            float x = Random.Range(-1f, 1f) * m_shakeIntensity;
+            float y = Random.Range(-1f, 1f) * m_shakeIntensity;
 
-            // 3
-            //float negativeX = Mathf.Abs(m_initialPosition.x) - 0.5f;
-            //float positiveX = Mathf.Abs(m_initialPosition.x) + 0.5f;
-            //Vector3 pointA = new Vector3(negativeX, transform.position.y, -10);
-            //Vector3 pointB = new Vector3(positiveX, transform.position.y, -10);
-            //transform.position = Vector3.Lerp(pointA, pointB, Time.deltaTime * m_smoothness);
+            if (m_shakeAxis == ShakeAxis.Xonly) { shakePosition = new Vector3(x, initialPosition.y, initialPosition.z); }
+            else if (m_shakeAxis == ShakeAxis.Yonly) { shakePosition = new Vector3(initialPosition.x, y, initialPosition.z); }
+            else if (m_shakeAxis == ShakeAxis.XY) { shakePosition = new Vector3(x, y, initialPosition.z); }
+            transform.position = shakePosition;
 
-            // 1
-            //float randomX = Random.Range(-0.5f, 0.5f);
-            //Vector3 pointA = transform.position;
-            //Vector3 pointB = new Vector3(randomX, 0, -10);
-            //transform.position = Vector3.Lerp(pointA, pointB, 0.5f);
+            elapsedShakingTime += Time.deltaTime;
 
-            m_elapsedShakingTime += Time.deltaTime;
             yield return null;
         }
-        this.transform.position = m_initialPosition;
+
+        this.transform.position = initialPosition;
     }
+    #endregion
 }
