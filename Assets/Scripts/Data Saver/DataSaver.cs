5fb89189d280204c3817c4b4ef06221679d9be47
Anis karam <anisTkaram@hotmail.com>
2024-09-30 23:00:47 +0300

    Update DataSaver
    - Updated and added the most used and common methods.

--- Assets/Scripts/Data Saver/DataSaver.cs
+++ Assets/Scripts/Data Saver/DataSaver.cs
@@ -3,57 +3,122 @@ using System.IO;
 
 public class DataSaver : MonoBehaviour
 {
-    public int index;
-    public int starsPerLevel;
+    #region Fields
+    private const string m_jsonFileName = "/playerdata.json";
+    #endregion
 
+
+    #region Unity Methods
+#if UNITY_EDITOR
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) { SaveData(); }
+        if (Input.GetKeyDown(KeyCode.S)) { SaveData(coins: 10, energy: 3, stars: 10, levelIndex: 0, starsPerLevel: 1); }
         if (Input.GetKeyDown(KeyCode.L)) { LoadData(); }
         if (Input.GetKeyDown(KeyCode.I)) { InitializePlayerData(); }
     }
+#endif
+    #endregion
 
+
+    #region Public Methods
+    public int GetCoins()
+    {
+        PlayerData playerData = LoadData();
+        return playerData.coins;
+    }
+    public int GetEnergy()
+    {
+        PlayerData playerData = LoadData();
+        return playerData.energy;
+    }
+    public int GetStars()
+    {
+        PlayerData playerData = LoadData();
+        return playerData.energy;
+    }
+    public int[] GetStarsForAllLevels()
+    {
+        PlayerData playerData = LoadData();
+        return playerData.arrayOfStarsPerLevel;
+    }
+    #endregion
+
+
+    #region Private Methods
+    /// <summary>
+    /// Initialize the PlayerData with default values
+    /// </summary>
     private void InitializePlayerData()
     {
-        string path = Application.persistentDataPath + "/playerData.json";
+        string path = Application.persistentDataPath + "/playerdata.json";
 
         if (File.Exists(path)) { return; }
 
+        // default data.
         PlayerData playerData = new PlayerData();
-        playerData.coins = 0;
-        playerData.energy = 5;
-        playerData.stars = 0;
-        playerData.arrayOfStarsPerLevel = new int[10];
+        playerData.coins = 0; // total amount of coins
+        playerData.energy = 5; // total and max amount of energy
+        playerData.stars = 0; // total amount of collected stars
+        playerData.arrayOfStarsPerLevel = new int[10]; // based on the amount of levels
 
         string contents = JsonUtility.ToJson(playerData);
         File.WriteAllText(path, contents);
+        Debug.Log($"Data initialized and saved: {contents}");
     }
-    private void SaveData()
-    {
-        string path = Application.persistentDataPath + "/playerData.json";
 
-        if (!File.Exists(path)) { return; }
+    /// <summary>
+    /// Saves the PlayerData in a JSON file.
+    /// </summary>
+    /// <param name="coins">Amount of coins to add</param>
+    /// <param name="energy">Amount of energy to assign</param>
+    /// <param name="stars">Amount of stars to add</param>
+    /// <param name="levelIndex">Current level index</param>
+    /// <param name="starsPerLevel">Amount of stars of the current level</param>
+    private void SaveData(int coins = -1, int energy = -1, int stars = -1, int levelIndex = -1, int starsPerLevel = -1)
+    {
+        string path = Application.persistentDataPath + m_jsonFileName;
+
+        // check if no json file found, we initialize player data then save.
+        if (!File.Exists(path)) { InitializePlayerData(); }
 
         string contents = File.ReadAllText(path);
         PlayerData playerData = JsonUtility.FromJson<PlayerData>(contents);
-        playerData.coins = 1;
-        playerData.energy = 1;
-        playerData.stars = 5;
-        playerData.arrayOfStarsPerLevel[index] = starsPerLevel;
+        if (coins >= 0) { playerData.coins += coins; } // add to the total coins.
+        if (stars >= 0) { playerData.stars += stars; } // add to the total stars.
+        if (energy >= 0) // add to the total energy.
+        {
+            playerData.energy = energy;
+            playerData.energy = Mathf.Clamp(playerData.energy, 0, 5); // ..min 0 and max 5
+        }
+        if (levelIndex >= 0 && starsPerLevel >= 0) // assign new amount of stars to the level.
+        {
+            if (starsPerLevel > playerData.arrayOfStarsPerLevel[levelIndex]) // ..only when the new amount of stars is greater than the saved one
+            {
+                playerData.arrayOfStarsPerLevel[levelIndex] = starsPerLevel;
+                playerData.arrayOfStarsPerLevel[levelIndex] = Mathf.Clamp(playerData.arrayOfStarsPerLevel[levelIndex], 0, 3); // ..min 0 and max 3
+            }
+        }
 
         string json = JsonUtility.ToJson(playerData);
         File.WriteAllText(path, json);
-        Debug.Log($"Loading... coins {playerData.coins}, stars {playerData.stars}, energy {playerData.energy}");
+        Debug.Log($"Data saved: {json}");
     }
 
-    private void LoadData()
+    /// <summary>
+    /// It loads the PlayerData JSON.
+    /// </summary>
+    /// <returns>Loaded PlayerData</returns>
+    private PlayerData LoadData()
     {
-        string path = Application.persistentDataPath + "/playerData.json";
+        string path = Application.persistentDataPath + m_jsonFileName;
 
-        if (!File.Exists(path)) { return; }
+        // check if no json file found, we initialize player data then load.
+        if (!File.Exists(path)) { InitializePlayerData(); }
 
         string contents = File.ReadAllText(path);
         PlayerData playerData = JsonUtility.FromJson<PlayerData>(contents);
-        Debug.Log($"Loading... coins {playerData.coins}, stars {playerData.stars}, energy {playerData.energy}");
+        Debug.Log($"Data loaded");
+        return playerData;
     }
+    #endregion
 }
